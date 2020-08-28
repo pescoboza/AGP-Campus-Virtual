9767a08725c66aa28decf0ac1823f166533eb3d8
lolo23jhon <lolo23jhon@gmail.com>
2020-08-27 19:29:11 -0700

    :construction: Added mock links to base template.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import flash, redirect, render_template
 
 from app import app
 from app.forms import LoginForm
@@ -10,5 +10,9 @@ def index():
 
 @app.route("/login",methods=["GET", "POST"])
 def login():
+    # TODO: Add database integration.
     form = LoginForm()
+    if form.validate_on_submit():
+        #flash("DEBUG: Login request for user {}.".format(form.username.data))
+        return redirect("/index")
     return render_template("login.html", title="Iniciar sesión", form=form)
