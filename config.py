4d7f2595343ff9a62f9f30e3f680cf0d5735b48f
Pedro Escoboza <lolo23jhon@gmail.com>
2020-08-30 18:00:27 -0700

    :wrench: Added email configuration. Removed exposed testing db info. Added debug environment variables.

--- config.py
+++ config.py
@@ -2,32 +2,52 @@ import os
 from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-MONGO_TEST_DB = "AGPCampusVirtualDev"
-MONGO_TEST_URI = "mongodb+srv://admin:fractalchargeimplosion@devcluster.k7j3g.mongodb.net/AGPCampusVirtualDev?retryWrites=true&w=majority"
 
 class Config(object):
     ENV = "production"
     DEBUG = False
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
+    # FlaskMongoengine settings
     MONGODB_SETTINGS = {
-        "db": os.environ.get("MONGO_DB") or MONGO_TEST_DB,
-        "host": os.environ.get("MONGO_URI") or MONGO_TEST_URI,
+        "db": os.environ.get("MONGO_DB"),
+        "host": os.environ.get("MONGO_URI"),
     }        
 
+    # FlaskLogin settings
     REMEMBER_COOKIE_DURATION = timedelta(minutes=15)
 
-    # Deprecated SQLalchemy code
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-    #     "sqlite:///" + os.path.join(basedir, "app.db")
-    # SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # FlaskMail settings
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
+    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL")
+    MAIL_SUPPRESS_SEND  = os.environ.get("MAIL_SUPPRESS_SEND")
+    MAIL_ASCII_ATACHMENTS = os.environ.get("MAIL_ASCII_ATACHMENTS")
+    MAIL_USERNAME  = os.environ.get("MAIL_USERNAME")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 
 class DevConfig(Config):
     ENV = "development"
     DEBUG = True
-    SECRET_KEY = "terces"
+    
+    SECRET_KEY = os.environ.get("DEBUG_SECRET_KEY")
 
+    # FlaskMongoengine settings
     MONGODB_SETTINGS = {
-        "db": MONGO_TEST_DB,
-        "host": MONGO_TEST_URI,
+        "db": os.environ.get("DEBUG_MONGO_DB"),
+        "host": os.environ.get("DEBUG_MONGO_URI"),
     }        
+
+    # FlaskMail settings
+    MAIL_SERVER = os.environ.get("DEBUG_MAIL_SERVER")
+    MAIL_PORT = os.environ.get("DEBUG_MAIL_PORT")
+    MAIL_USE_TLS = os.environ.get("DEBUG_MAIL_USE_TLS")
+    MAIL_USE_SSL = os.environ.get("DEBUG_MAIL_USE_SSL")
+    MAIL_SUPPRESS_SEND  = os.environ.get("DEBUG_MAIL_SUPPRESS_SEND")
+    MAIL_ASCII_ATACHMENTS = os.environ.get("DEBUG_MAIL_ASCII_ATACHMENTS")
+    MAIL_USERNAME  = os.environ.get("DEBUG_MAIL_USERNAME")
+    MAIL_DEFAULT_SENDER = os.environ.get("DEBUG_MAIL_DEFAULT_SENDER")
+    MAIL_PASSWORD = os.environ.get("DEBUG_MAIL_PASSWORD")
