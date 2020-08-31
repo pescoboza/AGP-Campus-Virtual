7bbd616d929bd3dceef727bf68b37c387b4110f1
Pedro Escoboza <lolo23jhon@gmail.com>
2020-08-30 18:27:37 -0700

    :wrench: Added helper to parse boolean env vars.

--- config.py
+++ config.py
@@ -3,6 +3,18 @@ from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Helper to take python boolean value from environment variables.
+def get_bool_env_var(varname):
+    value = os.environ.get(varname)
+    if value is None:
+        return None
+    if value == "true":
+        return True
+    if value == "false":
+        return False
+    raise AttributeError("Could not parse environment variable {} as boolean.".format(varname))
+
+# Production confituration
 class Config(object):
     ENV = "production"
     DEBUG = False
@@ -20,15 +32,15 @@ class Config(object):
     # FlaskMail settings
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = os.environ.get("MAIL_PORT")
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
-    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL")
-    MAIL_SUPPRESS_SEND  = os.environ.get("MAIL_SUPPRESS_SEND")
-    MAIL_ASCII_ATACHMENTS = os.environ.get("MAIL_ASCII_ATACHMENTS")
+    MAIL_USE_TLS = get_bool_env_var("MAIL_USE_TLS")
+    MAIL_USE_SSL = get_bool_env_var("MAIL_USE_SSL")
+    MAIL_SUPPRESS_SEND  = get_bool_env_var("MAIL_SUPPRESS_SEND")
+    MAIL_ASCII_ATACHMENTS = get_bool_env_var("MAIL_ASCII_ATACHMENTS")
     MAIL_USERNAME  = os.environ.get("MAIL_USERNAME")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
-
+# Development configuration
 class DevConfig(Config):
     ENV = "development"
     DEBUG = True
@@ -44,10 +56,10 @@ class DevConfig(Config):
     # FlaskMail settings
     MAIL_SERVER = os.environ.get("DEBUG_MAIL_SERVER")
     MAIL_PORT = os.environ.get("DEBUG_MAIL_PORT")
-    MAIL_USE_TLS = os.environ.get("DEBUG_MAIL_USE_TLS")
-    MAIL_USE_SSL = os.environ.get("DEBUG_MAIL_USE_SSL")
-    MAIL_SUPPRESS_SEND  = os.environ.get("DEBUG_MAIL_SUPPRESS_SEND")
-    MAIL_ASCII_ATACHMENTS = os.environ.get("DEBUG_MAIL_ASCII_ATACHMENTS")
+    MAIL_USE_TLS = get_bool_env_var("DEBUG_MAIL_USE_TLS")
+    MAIL_USE_SSL = get_bool_env_var("DEBUG_MAIL_USE_SSL")
+    MAIL_SUPPRESS_SEND  = get_bool_env_var("DEBUG_MAIL_SUPPRESS_SEND")
+    MAIL_ASCII_ATACHMENTS = get_bool_env_var("DEBUG_MAIL_ASCII_ATACHMENTS")
     MAIL_USERNAME  = os.environ.get("DEBUG_MAIL_USERNAME")
     MAIL_DEFAULT_SENDER = os.environ.get("DEBUG_MAIL_DEFAULT_SENDER")
     MAIL_PASSWORD = os.environ.get("DEBUG_MAIL_PASSWORD")
