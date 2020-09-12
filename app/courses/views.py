1f668187079b8fff48a0063b5470215b2ad757bc
pescoboza <lolo23jhon@gmail.com>
2020-09-11 20:46:19 -0700

    :truck: Removed old routes and changed views to match client requirements.

--- app/courses/views.py
+++ app/courses/views.py
@@ -4,84 +4,22 @@ from flask_login import login_required
 from . import courses
 from .forms import MultipleChoiceQuizForm
 
-@courses.route("/")
-def empty():
-    return redirect(url_for("courses.dashboard"))
-
-########################################################
-# Info routes
-########################################################
-@courses.route("/cancer-testicular/<page_name>")
+@courses.route("/cancer-testicular")
 def cancer_testicular(page_name):
-    return render_template("courses/cancer_testicular/{}.html".format(page_name.replace('-','_')))
+    return render_template("courses/cancer_testicular.html")
 
-@courses.route("/cancer-prostata/<page_name>")
+@courses.route("/cancer-prostata")
 def cancer_prostata(page_name):
-    return render_template("courses/cancer_prostata/{}.html".format(page_name.replace('-','_')))
+    return render_template("courses/cancer_prostata.html")
 
-@courses.route("/cancer-pulmon/<page_name>")
+@courses.route("/cancer-pulmon")
 def cancer_pulmon(page_name):
-    return render_template("courses/cancer_pulmon/{}.html".format(page_name.replace('-','_')))
+    return render_template("courses/cancer_pulmon.html")
 
-@courses.route("/cancer-mama/<page_name>")
+@courses.route("/cancer-mama")
 def cancer_mama(page_name):
-    return render_template("courses/cancer_mama/{}.html".format(page_name.replace('-','_')))
+    return render_template("courses/cancer_mama.html")
 
-@courses.route("/cancer-cervicouterino/<page_name>")
+@courses.route("/cancer-cervicouterino")
 def cancer_cervicouterino(page_name):
-    return render_template("courses/cancer_cervicouterino/{}.html".format(page_name.replace('-','_')))
-
-
-########################################################
-# Assignments views
-########################################################
-@courses.route("/dashboard")
-def dashboard():
-    modules = Module.objects().all()
-    return render_template("/courses/dashboard.html", modules=modules)
-
-# View for a course module
-@courses.route("/module")
-def module():
-    module_id = request.args.get("module_id")
-
-    module = Module.objects(id=module_id).first()
-    if module is None:
-        print("[DEBUG]: Module not found, id: {}".format(module_id))
-        return redirect(url_for("courses.dashboard"))
-
-    return render_template("courses/module.html", module=module)
-
-@courses.route("/task")
-@login_required
-def task():
-    module_id = request.args.get("module_id")
-    task_num  = int(request.args.get("task_num"))
-    
-    if module_id is None or \
-        task_num is None or \
-        task_num < 0:
-        return redirect(url_for("courses.dashboard"))
-
-    module = Module.objects(id=module_id).first()
-    if module is None or \
-        task_num >= len(module.tasks):
-        return redirect(url_for("courses.dashboard"))
-    
-    task = None
-    try:
-        task = module.tasks[task_num]
-    except IndexError:
-        pass
-
-    if task is None:
-        return redirect(url_for("courses.dashboard"))
-    
-
-    form = None
-    if task._cls == "MultipleChoiceQuiz":
-        form = MultipleChoiceQuizForm.from_mongo_obj(task)
-
-    
-
-    return render_template("courses/task.html", form=form)
+    return render_template("courses/cancer_cervicouterino.html")
