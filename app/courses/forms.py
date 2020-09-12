63b38c81100d85be4c257fd01c6f44880903d9d5
pescoboza <lolo23jhon@gmail.com>
2020-09-11 19:01:08 -0700

    :construction: Scrapped MultipleChoiceQuiz and Task mongoengine models and started rewriting form interface to resurrect mongo objects as a form.

--- app/courses/forms.py
+++ app/courses/forms.py
@@ -1,7 +1,8 @@
+from random import shuffle
 from flask_wtf import FlaskForm
 from wtforms import RadioField, FieldList, FormField, SubmitField
 
-from ..models import MultipleChoiceQuiz, MultipleChoiceQuestion
+from ..models import MultipleChoiceQuiz, MultipleChoiceQuestion, QUESTION_TOPICS
 
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -17,12 +18,28 @@ class MultipleChoiceQuestionForm(FlaskForm):
 class MultipleChoiceQuizForm(FlaskForm):
     questions = FieldList(FormField(MultipleChoiceQuestionForm))
 
-    submit = SubmitField("Entregar examen")
+    submit = SubmitField("Entregar evaluación")
 
     @staticmethod
-    def from_mongo_obj(obj):
+    def from_mongo_obj(questions):
         form = MultipleChoiceQuizForm()
-        for q in obj.questions:
+        for q in questions:
             form.questions.append_entry(MultipleChoiceQuestionForm.from_mongo_obj(q))
 
         return form
+    
+    @staticmethod
+    def generate_random_quiz(topic, num_questions):
+
+        # Validate the topic.        
+        if topic not in QUESTION_TOPICS:
+            raise ValueError("Invalid quiz topic.")
+
+        
+        questions = MultipleChoiceQuestion.objects(topic=topic)
+
+        if len(questions) < num_questions:
+            raise ValueError("Not enough questions of that topic were found.")
+        
+        shuffle(questions)
+        questions = questions.slice([0, num_questions])
