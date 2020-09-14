2c87b4173908a9ed55ab1d67bf5008c1f65fcf5c
pescoboza <lolo23jhon@gmail.com>
2020-09-13 18:38:16 -0700

    :lipstick: Added javascript to hide collapsible button for quiz.

---
+++ app/static/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("button-collapse-quiz").onclick = () => document.getElementById("button-collapse-quiz").hidden = true;
+
+function hide_button_collapse_quiz() {
+    document.getElementById("button-collapse-quiz").hidden = true;
+}
