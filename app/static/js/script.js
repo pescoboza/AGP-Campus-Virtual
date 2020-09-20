45b5c1bb0d7a7c8939c1608ee3b6217405afb63f
Pedro Escoboza <lolo23jhon@gmail.com>
2020-09-19 19:11:13 -0700

    :truck: Moved javascript static scipt to base.html.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,9 @@
-document.getElementById("button-collapse-quiz").onclick = () => document.getElementById("button-collapse-quiz").hidden = true;
+// document.getElementById("button-collapse-quiz").onclick = () => document.getElementById("button-collapse-quiz").hidden = true;
 
-function hide_button_collapse_quiz() {
-    document.getElementById("button-collapse-quiz").hidden = true;
+let button_collapse_quiz = document.getElementById("button-collapse-quiz");
+button_collapse_quiz.onclick = () => button_collapse_quiz.hidden = true;
+
+let close_btns = document.getElementsByClassName("close")
+for (cb of close_btns) {
+    cb.onclick = () => cb.parentElement.hidden = true;
 }
