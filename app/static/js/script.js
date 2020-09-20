7a0106fc007456ec63de9efca349eedf9f584527
Pedro Escoboza <lolo23jhon@gmail.com>
2020-09-19 20:37:52 -0700

    :lipstick: Added javascript to close flashed messages when clicking the cross button.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,9 +1,17 @@
-// document.getElementById("button-collapse-quiz").onclick = () => document.getElementById("button-collapse-quiz").hidden = true;
-
+// Hide button from quiz after first click.
 let button_collapse_quiz = document.getElementById("button-collapse-quiz");
 button_collapse_quiz.onclick = () => button_collapse_quiz.hidden = true;
 
-let close_btns = document.getElementsByClassName("close")
-for (cb of close_btns) {
-    cb.onclick = () => cb.parentElement.hidden = true;
+
+function hideParent(e) {
+    let ele = e.target;
+    ele.parentElement.hidden = true;
+}
+
+// Hide flashed messages when clicking on cross.
+let flashes = document.getElementsByClassName("aler");
+let num_flashes = flashes.length;
+for (let i = 0; i < num_flashes; i++) {
+    flashes[i].addEventListener("click", hideParent);
+    console.log(flashes[i]);
 }
