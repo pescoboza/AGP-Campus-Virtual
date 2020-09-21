141586f33d41d405637da2006ed16403362de1e1
Pedro Escoboza <lolo23jhon@gmail.com>
2020-09-20 22:10:21 -0700

    :sparkles: Added printing of pdf files from jinja templates with integrated css.

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+
 from flask import flash, redirect, render_template, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 
@@ -18,3 +19,15 @@ def contact():
 @main.route("/faq")
 def faq():
     return render_template("main/faq.html")
+
+
+@main.route("/certificate")
+@login_required
+def certificate():
+    import pdfkit
+    from .. import pdfkit_config
+
+    rendered = render_template("certificate/certificate.html")
+    pdf = pdfkit.from_string(
+        rendered, "out.pdf", css="app/templates/certificate/certificate.css", configuration=pdfkit_config)
+    return rendered
