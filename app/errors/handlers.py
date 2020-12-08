943a0006649c04c9d066539a8e5dc56881e04cd3
pescoboza <psebastian01@hotmail.com>
2020-12-08 09:41:44 -0700

    :sparkles: Added 404 and 500 error handler pages.

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -4,10 +4,10 @@ from . import errors
 
 
 
-@errors.errorhandler(404)
+@errors.app_errorhandler(404)
 def not_found_error(error):
     return (render_template("errors/404.html"), 400)
 
-@errors.errorhandler(500)
+@errors.app_errorhandler(500)
 def internal_error(error):
     return (render_template("errors/500.html"),500)
